2f77596416e1ca73183b39b7eb4648d6137896a2
mertins <luciano@mertins.com.br>
2024-05-22 11:37:10 -0300

    Iniciando

---
+++ Generic/src/main/java/br/com/mm/dlj/generic/Generic.java
@@ -0,0 +1,24 @@
+package br.com.mm.dlj.generic;
+
+import org.knowm.xchart.BitmapEncoder;
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.XYChart;
+
+/**
+ *
+ * @author mertins
+ */
+public class Generic {
+
+    private void grafico() {
+        double[] xData = new double[]{0.0, 1.0, 2.0};
+        double[] yData = new double[]{2.0, 1.0, 0.0};
+
+        XYChart chart = QuickChart.getChart("Sample Chart", "X", "Y", "y(x)", xData, yData);
+        BitmapEncoder.getBufferedImage(chart);
+    }
+
+    public static void main(String[] args) {
+        new Generic().grafico();
+    }
+}
